a78bbd10bc87cef732c6b09d3bc6ac0b2cfe003f
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-08-21 19:28:32 +0300

    allow back max_tokens+temperature

--- methods/callbacks.py
+++ methods/callbacks.py
@@ -523,8 +523,8 @@ class Method:  # pylint: disable=E1101,R0903,W0201
                 "ai_model_params": {
                     "model": model,
                     #
-                    # "temperature": settings["settings"]["temperature"],
-                    # "max_tokens": settings["settings"]["max_tokens"],
+                    "temperature": settings["settings"]["temperature"],
+                    "max_tokens": settings["settings"]["max_tokens"],
                     #
                     **auth_kwargs,
                 },
@@ -535,8 +535,8 @@ class Method:  # pylint: disable=E1101,R0903,W0201
             "ai_model_params": {
                 "model": model,
                 #
-                # "temperature": settings["settings"]["temperature"],
-                # "max_tokens": settings["settings"]["max_tokens"],
+                "temperature": settings["settings"]["temperature"],
+                "max_tokens": settings["settings"]["max_tokens"],
                 #
                 **auth_kwargs,
             },
